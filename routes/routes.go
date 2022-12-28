fc21b4c6b3b18f0b9b66f758d8c94a6eb1c68db4
Gabriel <gabriel@MacBook.local>
2022-12-27 21:41:02 -0800

    modificacao do modulo

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,12 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Gabriel-Newton-dev/gin-api-rest/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 func HandleRequest() {
-
 	r := gin.Default()
-	r.GET("/alunos", controllers.ExibeTodosAlunos) //Temos uma rota r, e especificamos o verbo GET(pegar)irá retornar / colocando endpoint /alunos
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.Run()
 }
