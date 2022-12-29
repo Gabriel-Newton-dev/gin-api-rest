292cc4fecafe6816cb9c78d54f66de1b1e581397
Gabriel <gabriel@MacBook.local>
2022-12-28 20:13:26 -0800

    Criacao da struct alunos na pasta models

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"github.com/Gabriel-Newton-dev/gin-api-rest/models"
 	"github.com/Gabriel-Newton-dev/gin-api-rest/routes"
 )
 
 func main() {
-
+	models.Alunos = []models.Aluno{
+		{Nome: "Gabriel", CPF: "123.456.123-31", RG: "12934234-1"},
+		{Nome: "Guilheme", CPF: "345.456.432-43", RG: "32456432-1"},
+	}
 	routes.HandleRequest()
 }
