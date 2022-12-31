87d9cb8c9c46eb576926459057b6c761f5ee0417
Gabriel <gabriel@MacBook.local>
2022-12-30 20:26:47 -0800

    modificacao da func database

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/Gabriel-Newton-dev/gin-api-rest/database"
 	"github.com/Gabriel-Newton-dev/gin-api-rest/models"
 	"github.com/Gabriel-Newton-dev/gin-api-rest/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Gabriel", CPF: "123.456.123-31", RG: "12934234-1"},
 		{Nome: "Guilheme", CPF: "345.456.432-43", RG: "32456432-1"},
