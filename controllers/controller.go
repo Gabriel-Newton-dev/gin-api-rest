75cd0f906f07db1aff19d7bfeb9d54b9991bb646
Gabriel <gabriel@MacBook.local>
2023-01-09 12:51:11 -0800

    Criacao da funcao Edita Aluno e criado respectivo Endpoint

--- controllers/controller.go
+++ controllers/controller.go
@@ -53,3 +53,21 @@ func DeletaAluno(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"Deletado": "Usuário deletado do banco de dados com sucesso."})
 }
+
+func EditaAluno(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.First(&aluno, id)
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Erro na função Edita Aluno"})
+		return // caso tenha um erro ele irá sair da função.
+	}
+	database.DB.Model(&aluno).UpdateColumns(aluno)
+
+}
+
+// a funcao edita alunos ela irá pegar o copor da nossa requisicao e mudar no BD
+// Se tem o corpo a forma que usamos é o shouldBindJson para empacotar todo corpo da
+//requisicao com base na nossa struct, com base no nosso endereco de memória da var aluno que criamos
+// if err := .ShouldBindJSON()
