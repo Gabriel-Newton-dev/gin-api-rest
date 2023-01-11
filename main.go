3b75e1185f52b0dad68f72f276859a5d0c752a89
Gabriel <gabriel@MacBook.local>
2023-01-10 19:47:54 -0800

    Configurado variavel de ambiente privada para conexao do DB

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"github.com/Gabriel-Newton-dev/gin-api-rest/database"
 	"github.com/Gabriel-Newton-dev/gin-api-rest/routes"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
 	database.ConectaComBancoDeDados()
 	routes.HandleRequests()
 }
