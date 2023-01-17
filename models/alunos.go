13e61ba01902cd2c5a7a10e6addf2e44a1c72eb7
Gabriel <gabriel@MacBook.local>
2023-01-17 11:32:27 -0800

    [FEAT] Inclusao de validador na struct Aluno

--- models/alunos.go
+++ models/alunos.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type Aluno struct {
 	gorm.Model
-	Nome string `json:"nome"`
-	CPF  string `json:"cpf"`
-	RG   string `json:"rg"`
+	Nome string `json:"nome" validate:"nonzero"`
+	CPF  string `json:"cpf" validate:"len=11"`
+	RG   string `json:"rg" validate:"len=9"`
 }
 
 // criei um slice de Aluno(como se fosse uma lista, pq terá vários)
