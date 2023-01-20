c86affbc355b7a05184039bba8b513f6fcc74a2e
Gabriel <gabriel@MacBook.local>
2023-01-19 20:25:15 -0800

    [FEAT] Modificacao da funcao de teste 'TestVerificaEndpointSaudacao'

--- controllers/controller.go
+++ controllers/controller.go
@@ -19,8 +19,7 @@ func ExibeTodosAlunos(c *gin.Context) {
 func Saudacao(c *gin.Context) {
 	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
-		"API diz": "Seja bem-vindo " + nome + ", a nossa API que se comunica com banco de dados postgres?",
-	})
+		"API diz": "Seja bem-vindo " + nome + " a nossa API"})
 }
 
 func CriaNovoAluno(c *gin.Context) {
