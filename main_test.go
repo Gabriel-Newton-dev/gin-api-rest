9d426ed4187cc5e0541f6fc10d1f1267ecd33d01
Gabriel <gabriel@MacBook.local>
2023-01-21 12:11:59 -0800

    [CREATE] Criacao da funcao de teste para listar alunos

--- main_test.go
+++ main_test.go
@@ -7,7 +7,9 @@ import (
 	"testing"
 
 	"github.com/Gabriel-Newton-dev/gin-api-rest/controllers"
+	"github.com/Gabriel-Newton-dev/gin-api-rest/database"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,3 +30,22 @@ func TestVerificaEndpointSaudacao(t *testing.T) {
 	assert.Equal(t, mockDaResposta, string(responseBody))
 
 }
+
+func TestListandoTodosAlunosHandler(t *testing.T) {
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+	database.ConectaComBancoDeDados()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	response := httptest.NewRecorder() // para armanezar todas as informacoes do corpo da nossa resposta
+	r.ServeHTTP(response, req)
+	assert.Equal(t, http.StatusOK, response.Code) // assertação para verificar se de fato estamos recebendo o status code que a gente espera.
+	// t- teste, depois valor esperado - http.StatusOK, e o que vou receber é o status code da resposta
+}
+
+// func TestVerificaEdndpointCriaAluno(t *testing.T) {
+// 	r := SetupDasRotasDeTeste()
+// 	r.POST("/alunos", controllers.CriaNovoAluno)
+// 	req,
+// }
