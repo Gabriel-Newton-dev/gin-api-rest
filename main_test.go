2a0b8a18382346528d2e3f3b2e85bd3c2c3bf942
Gabriel <gabriel@MacBook.local>
2023-01-21 13:56:40 -0800

    [CREATION] Criado Func para Criar e Deletar alunos Mock

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -8,14 +9,30 @@ import (
 
 	"github.com/Gabriel-Newton-dev/gin-api-rest/controllers"
 	"github.com/Gabriel-Newton-dev/gin-api-rest/database"
+	"github.com/Gabriel-Newton-dev/gin-api-rest/models"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupDasRotasDeTeste() *gin.Engine {
 	rotas := gin.Default()
 	return rotas
+}
+
+func CriarAlunoMock() {
+	aluno := models.Aluno{Nome: "Aluno Teste", CPF: "12345678910", RG: "123456789"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+
+}
+
+func DeletarAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
+
 }
 
 func TestVerificaEndpointSaudacao(t *testing.T) {
@@ -35,17 +52,14 @@ func TestListandoTodosAlunosHandler(t *testing.T) {
 	viper.SetConfigFile(".env")
 	viper.ReadInConfig()
 	database.ConectaComBancoDeDados()
+	CriarAlunoMock()
+	defer DeletarAlunoMock()
 	r := SetupDasRotasDeTeste()
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	req, _ := http.NewRequest("GET", "/alunos", nil)
 	response := httptest.NewRecorder() // para armanezar todas as informacoes do corpo da nossa resposta
 	r.ServeHTTP(response, req)
 	assert.Equal(t, http.StatusOK, response.Code) // assertação para verificar se de fato estamos recebendo o status code que a gente espera.
+	fmt.Println(response.Body)
 	// t- teste, depois valor esperado - http.StatusOK, e o que vou receber é o status code da resposta
 }
-
-// func TestVerificaEdndpointCriaAluno(t *testing.T) {
-// 	r := SetupDasRotasDeTeste()
-// 	r.POST("/alunos", controllers.CriaNovoAluno)
-// 	req,
-// }
