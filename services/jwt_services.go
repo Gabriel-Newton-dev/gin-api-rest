54aefd63eefa963a6c7f898f2e778acb723592ed
Gabriel <gabriel@MacBook.local>
2023-03-29 11:26:33 -0700

    cretead login_controllers

--- services/jwt_services.go
+++ services/jwt_services.go
@@ -1,13 +1,56 @@
 package services
 
-// type jwtService struct {
-// 	secretkey string
-// 	issure    string //quem está assiando o nosso token
-// }
+import (
+	"fmt"
+	"time"
 
-// func NewJwtService() *jwtService {
-// 	return &jwtService{
-// 		secretKey: "secret-key",
-// 		issure:    "gin=api",
-// 	}
-// }
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type jwtService struct {
+	secretkey string
+	issure    string //quem está assiando o nosso token
+}
+
+func NewJwtService() *jwtService {
+	return &jwtService{
+		secretkey: "123456",
+		issure:    "gin=api",
+	}
+}
+
+type Claim struct {
+	Sum uint `json:"sum"`
+	jwt.StandardClaims
+}
+
+// funcao de gerar token // retorna uma string que será o nosso token ou um error
+func (s *jwtService) GenerateToken(id uint) (string, error) {
+	claim := &Claim{
+		id,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 2).Unix(),
+			Issuer:    s.issure,
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	t, err := token.SignedString([]byte(s.secretkey))
+	if err != nil {
+		return "", err
+	}
+	return t, nil
+}
+
+// validar o token
+
+func (s *jwtService) Validatetoken(token string) bool {
+	_, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, isValid := t.Method.(*jwt.SigningMethodHMAC); !isValid {
+			return nil, fmt.Errorf("Invalid token: %v", token)
+		}
+		return []byte(s.secretkey), nil
+	})
+	return err == nil
+}
