8d15b771e3b6c586f899c5531b5cbb69c134da4c
Gabriel <gabriel@MacBook.local>
2023-04-08 00:19:22 -0700

    [FEAT] Created services healthcheck

---
+++ services/healthcheck.go
@@ -0,0 +1,13 @@
+package services
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Healthcheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"": "Working",
+	})
+}
--- services/jwt_service_test.go
+++ services/jwt_service_test.go
@@ -2,8 +2,16 @@ package services
 
 import (
 	"testing"
+
+	"github.com/golang/mock/gomock"
 )
 
 func Test_GenerateToken(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	m := newMockGenerateToken(ctrl)
+
+	m.
+		EXPECT()
 
 }
